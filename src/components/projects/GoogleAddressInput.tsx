4f6ca0d1bd69d47aa28fe956e5d8105e195f62f2
R3v3ill3 <troy@reveille.net.au>
2025-11-05 17:07:30 +1000

    useCallback callback

--- src/components/projects/GoogleAddressInput.tsx
+++ src/components/projects/GoogleAddressInput.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { toast } from "sonner";
