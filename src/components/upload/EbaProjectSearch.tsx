cf346966af2974b9fd064ca7d9b3ca6a5e9731d3
R3v3ill3 <troy@reveille.net.au>
2026-01-17 08:01:29 +1000

    eba project search fix

--- src/components/upload/EbaProjectSearch.tsx
+++ src/components/upload/EbaProjectSearch.tsx
@@ -14,6 +14,7 @@ import { getSupabaseBrowserClient } from '@/lib/supabase/client';
 import { useToast } from "@/components/ui/use-toast";
 import { Checkbox } from '@/components/ui/checkbox';
 import { getTradeOptionsByStage, getAllStages, getStageLabel } from "@/utils/tradeUtils";
+import { useKeyContractorTradesSet } from '@/hooks/useKeyContractorTrades';
 
 interface ProjectEmployer {
   id: string;
